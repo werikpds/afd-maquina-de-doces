cfd425b352cbbfc15546f999e6f8591c9edcd138
Werik Pinheiro <werikpds@gmail.com>
2021-05-23 11:59:24 -0300

    Penultima subida

---
+++ analisador.py
@@ -0,0 +1,204 @@
+import sys
+ALFABETO = "a b c d e f g h i j k l m n o p q r s t u v x y z A B C D E F G J I J K L M N O P Q R S T U V X Y Z"
+NUMEROS = "0123456789."
+COMPARE = "<>=!"
+
+FRASE_OCO = "Verifique a quantidade de ocorrências"
+
+sys.setrecursionlimit(4000)
+
+
+def analise(bloco):
+    bloco_otimizado = bloco.replace(' ', '')
+    retorno = lex(bloco_otimizado)
+    erros = identificador_de_erros(retorno)
+    if erros != None: return {"retorno": erros}
+    return {"retorno": retorno}
+
+
+def lex(bloco, tokens=[], lendo=""):
+    """if bloco[0] == "s" and lendo == "":
+        return lex(bloco[1:], tokens, lendo = "s")
+    elif bloco[0] == "e" and lendo == "s":
+        return lex(bloco[1:], tokens, lendo = "se")"""
+    if bloco[0] == "(" and lendo == "se":
+        tokens.append(plv_reserv("se"))
+        tokens.append(simbolo("("))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+    if bloco[0] == "(" and lendo == "enquanto":
+        tokens.append(plv_reserv("enquanto"))
+        tokens.append(simbolo("("))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+    elif bloco[0] == ")" and lendo != "":
+        try:
+            float(lendo)
+            tokens.append(numerico(lendo))
+            tokens.append(simbolo(")"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            tokens.append(simbolo(")"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+    elif bloco[0] == "(" and lendo != "":
+        try:
+            float(lendo)
+            tokens.append(numerico(lendo))
+            tokens.append(simbolo("("))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            tokens.append(simbolo("("))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+    elif bloco[0] == ";" and lendo == "":
+        try:
+            tokens.append(simbolo(";"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            tokens.append(simbolo(";"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+    elif bloco[0] == ";" and lendo != "":
+        try:
+            float(lendo)
+            tokens.append(numerico(lendo))
+            tokens.append(simbolo(";"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            tokens.append(simbolo(";"))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+    elif bloco[0] == "\"" and lendo != "":
+        try:
+            float(lendo)
+            tokens.append(numerico(lendo))
+            tokens.append(simbolo("\""))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            tokens.append(simbolo("\""))
+            try:
+                return lex(bloco[1:], tokens, lendo="")
+            except:
+                return tokens
+    elif bloco[0] == "=" and lendo in COMPARE:
+        tokens.append(simbolo(lendo + bloco[0]))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+    elif lendo in COMPARE and lendo != "":
+        tokens.append(simbolo(lendo + bloco[0]))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+
+    elif bloco[0] in COMPARE and lendo != "":
+        try:
+            float(lendo)
+            tokens.append(numerico(lendo))
+            try:
+                return lex(bloco[1:], tokens, lendo=bloco[0])
+            except:
+                tokens.append(simbolo(bloco[0]))
+                return tokens
+        except:
+            tokens.append(identificadores(lendo))
+            try:
+                return lex(bloco[1:], tokens, lendo=bloco[0])
+            except:
+                tokens.append(simbolo(bloco[0]))
+                return tokens
+    elif bloco[0] == "(" and lendo == "":
+        tokens.append(simbolo("("))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+    elif bloco[0] == ")" and lendo == "":
+        tokens.append(simbolo(")"))
+        try:
+            return lex(bloco[1:], tokens, lendo="")
+        except:
+            return tokens
+    elif ((bloco[0] in ALFABETO) or (bloco[0] in NUMEROS)):
+        try:
+            return lex(bloco[1:], tokens, lendo=lendo + bloco[0])
+        except:
+            try:
+                float(lendo + bloco[0])
+                tokens.append(numerico(lendo + bloco[0]))
+                return tokens
+            except:
+                tokens.append(identificadores(lendo + bloco[0]))
+                return tokens
+
+
+def simbolo(simbolo):
+    return (simbolo, "simbolo")
+
+
+def plv_reserv(plv_reserv):
+    return (plv_reserv, "palavra reservada")
+
+
+def identificadores(id):
+    return (id, "identificador")
+
+
+def numerico(num):
+    return (num, "numerico")
+
+
+def identificador_de_erros(entrada):
+    if entrada[len(entrada)-1][0] != ";":
+        return [(";", "Falta no final do bloco")]
+    elif (entrada.count((")", "simbolo")) != entrada.count(("(", "simbolo"))):
+        return [("(  )", FRASE_OCO)]
+    elif entrada.count(("\"", "simbolo"))%2 == 0:
+        return [("  \"  ", "String Quebrada")]
+    """
+    if entrada.count(("\"", "simbolo")) == entrada.count(("\"", "simbolo")):
+        return [("  \"  ", "String Quebrada")]
+    """
+
+
+    return None
+
+
+print(analise("enquanto(12.3a!=Teste\");"))
+# print("Teste do Teste"[2:])
+
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from afd import afd
 from elevador import vai_para
 from flask_cors import CORS
+from analisador import analise
 
 app = Flask(__name__)
 CORS(app)
@@ -21,6 +22,12 @@ def elevador():
     data = request.get_json()
     print(data["andares"])
     return vai_para(data["andares"]) , 200
+
+@app.route('/analisador', methods=['POST'])
+def analisador():
+    data = request.get_json()
+    print(data["bloco"])
+    return analise(data["bloco"]) , 200
 
 if __name__ == '__main__':
     app.run(debug=True)
